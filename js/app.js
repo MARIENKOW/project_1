9de4612b101db60b71fe557a9211ac333ac49aac
MARIENKOW <den.marienkow@gmail.com>
2022-10-15 03:51:26 +0300

    added footer and fixed old bugs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       dots:true,
       // adaptiveHeight:true,
       slidesToShow:5,
-      slidesToScroll:3,
+      slidesToScroll:2,
       speed:500,
       easing:'ease-in-out',
       infinite:true,
@@ -21,7 +21,7 @@ $(document).ready(function(){
             breakpoint:768,
             settings:{
                slidesToShow :2,
-               slidesToScroll:1,
+               slidesToScroll:2,
             }
          },{
             breakpoint:400,
@@ -32,39 +32,54 @@ $(document).ready(function(){
       ]
    });
 });
+//! ///---jQuery---\\\ !\\
+
+/*! ///---jQuery---\\\ !*/
 const searchSvg = document.querySelector('.header__btn');
 const headerInner = document.querySelector('.header__inner');
 const body = document.querySelector('body');
 const burger = document.querySelector('.header__burger')
-const headerUp = document.querySelector('.header--up');
 const header = document.querySelector('.header');
 const headerGender = document.querySelector('.header__gender');
-const headerUpHeight = headerUp.offsetHeight;
 const intro = document.querySelector('.intro__inner');
-let on = null;
 let i = null;
 let x = 0;
 function headerInnerSearch(){
-   if(searchSvg){
-      if(headerInner && !headerInner.classList.contains('_searchActive'))
+   if(searchSvg && headerInner && !headerInner.classList.contains('_searchActive') && window.innerWidth<=768){
          searchSvg.addEventListener('click',function(){
             headerInner.classList.add('_searchActive')
          })
    }
 }
-
-if(burger){
-   burger.addEventListener('click',function(){
-      if(headerInner){
-         headerInner.classList.toggle('_burgerOpen')
-         if(headerInner.classList.contains('_burgerOpen')){
-            body.style.overflow = 'hidden';
-         }else{
-            body.style.overflow = '';
-         }
-      }
-   })
+//! ///---SEARCH ACTIVE---\\\ !\\
+function scrollSearch(){
+   x++;
+   if(headerInner && headerInner.classList.contains('_searchActive') && x >= 50){headerInner.classList.remove('_searchActive')}
 }
+//! ///---SCROLL SEARCH---\\\ !\\
+setInterval(function(){ 
+   let y = window.scrollY;
+   if(i === y){
+      x = 0;
+   }
+   i = y;
+},500)
+//! ///---SCROLL STOP---\\\ !\\
+const burgerOpen = () =>{
+   if(burger){
+      burger.addEventListener('click',function(){
+         if(headerInner){
+            headerInner.classList.toggle('_burgerOpen')
+            if(headerInner.classList.contains('_burgerOpen')){
+               body.style.overflow = 'hidden';
+            }else{
+               body.style.overflow = '';
+            }
+         }
+      })
+   }
+}
+//! ///---BURGER OPEN/CLOSE---\\\ !\\
 let _slideUpFlex = (target,duration = 500) => {
    if (!target.classList.contains('_a')){
       target.classList.add('_a');
@@ -85,97 +100,87 @@ let _slideToggleFlex = (target,duration = 500) => {
       return _slideUpFlex(target,duration);
    }
 }
+let spoilerDot = null
 function spoilerFlex(){
    let sliders = document.querySelectorAll('._slider');
-   for(let i=0;i<sliders.length;i++){
-      let slider = sliders[i];
-      slider.nextElementSibling.classList.add('_a');
-      slider.nextElementSibling.style.height = '0px';
-      slider.addEventListener('click',function copyrightClick(){
-            if(slider.classList.contains('_slider')){
-               slider.parentElement.classList.toggle('_sliderOpen');
-            _slideToggleFlex(slider.nextElementSibling);
-            }
+   if(window.innerWidth<=768){
+      for(let i=0;i<sliders.length;i++){
+         let slider = sliders[i];
+         slider.classList.add('_sliderRun')
+         slider.nextElementSibling.classList.add('_a');
+         slider.nextElementSibling.style.height = '0px';
+         slider.parentElement.classList.remove('_sliderOpen');
+         if(spoilerDot!=1){
+            slider.addEventListener('click',function copyrightClick(){
+                  if(slider.classList.contains('_slider') && slider.classList.contains('_sliderRun')){
+                     slider.parentElement.classList.toggle('_sliderOpen');
+                  _slideToggleFlex(slider.nextElementSibling);
+                  }
+            })
+         }
+      }
+      spoilerDot = 1;
+   }else{
+      for(let i=0;i<sliders.length;i++){
+         let slider = sliders[i];
+         slider.classList.remove('_sliderRun')
+         slider.nextElementSibling.classList.remove('_a');
+         // slider.parentElement.classList.remove('_sliderOpen');
+         slider.nextElementSibling.style.height = '';
+      }
+   }
+}
+//! ///---SPOILER OPEN/CLOSE---\\\ !\\
+const swipe = document.querySelector('.intro__swipe');
+const slider = document.querySelector('.slider--up');
+const swipeClick = () =>{
+   if (swipe && slider){
+      swipe.addEventListener('click',function(){
+         slider.scrollIntoView({
+            behavior: 'smooth',
+            block: "start",
+            inline: "nearest"
+         });
       })
    }
 }
-
-setInterval(function(){ 
-   let y = window.scrollY;
-   if(i === y){
-      x = 0;
-   }
-   i = y;
-},500)
-function genderHight(){
-   if(window.innerWidth<=768){
-      const headerHeight = header.offsetHeight;
-      const headerTop = header.getBoundingClientRect().top
-      if(headerTop != 0 ){
-         const headerGenderHight = headerHeight+headerUpHeight;
-         headerGender.style.height = `calc(100vh - ${headerGenderHight}px)`;
+//! ///---SMOOTH SCROLL---\\\ !\\
+const headerFixed = () =>{
+   if(header){
+      if(window.scrollY>=20){
+         header.classList.add('_fixed');
       }else{
-         headerGender.style.height = `calc(100vh - (${headerHeight}px))`;
+         header.classList.remove('_fixed');
       }
-   }else{
-      headerGender.style.height = ``;
    }
 }
-const shipSpoilers = document.querySelectorAll('.ship__btn')
-function addSliders(){
-   for(let shipSpoiler of shipSpoilers ){
-      shipSpoiler.classList.add('_slider')
-   }
-}
-function windowWidth(){
-   if (window.innerWidth<=768){
-      addSliders();
-      if (on != 1){
-         on = 1;
-         headerInnerSearch();
-         spoilerFlex();
-         window.addEventListener('scroll',function scrollSearch(){
-            genderHight();
-            x++;
-            if(headerInner && headerInner.classList.contains('_searchActive') && x >= 50){headerInner.classList.remove('_searchActive')}
-         })
+//! ///---HEADER FIXED---\\\ !\\
+const swipeHiden = () =>{
+   if(swipe){
+      const partWindow = (window.innerHeight/4)*3
+      if(swipe.getBoundingClientRect().top<= partWindow){
+         swipe.classList.add('_hiden')
+      }else{
+         swipe.classList.remove('_hiden')
       }
-   }else{
-      // for(let shipSpoiler of shipSpoilers ){
-      //    shipSpoiler.classList.remove('_slider')
-      //    shipSpoiler.nextElementSibling.style.height = '';
-
-      // }
    }
 }
-genderHight();
-windowWidth();
-window.addEventListener('resize',function(){
-   windowWidth();
-   genderHight();
-})
-const swipe = document.querySelector('.intro__swipe');
-const slider = document.querySelector('.slider--up');
-if (swipe){
-   swipe.addEventListener('click',function(){
-      slider.scrollIntoView({
-         behavior: 'smooth',
-         block: "start",
-         inline: "nearest"
-      });
-   })
-}
+//! ///---BTN HIDEN/VISIBLE---\\\ !\\
+swipeClick(); //smoothh scroll
+burgerOpen(); //burger open/close
+spoilerFlex(); //ADD spoilers
+headerInnerSearch()
+//! ///---FUNCTIONS CALL---\\\ !\\
 window.addEventListener('scroll',function(){
-   const partWindow = (window.innerHeight/4)*3
-   if(swipe.getBoundingClientRect().top<= partWindow){
-      swipe.classList.add('_hiden')
-   }else{
-      swipe.classList.remove('_hiden')
-   }
-   console.log(this.window.scrollY);
-   if(this.window.scrollY>=20){
-      header.classList.add('_fixed');
-   }else{
-      header.classList.remove('_fixed');
+   swipeHiden();
+   headerFixed();
+   scrollSearch();
+})
+//! ///---FUNCTIONS CALL ON SCROLL---\\\ !\\
+window.addEventListener('resize',function(){
+   spoilerFlex();
+   if(window.innerWidth<=768){
+      body.style.overflow = '';
    }
 })
+//! ///---FUNCTION CALL ON RESIZE---\\\ !\\
