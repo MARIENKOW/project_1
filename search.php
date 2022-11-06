1906454153a5932fb272a8614c9e629182577ce6
MARIENKOW <den.marienkow@gmail.com>
2022-11-07 01:38:05 +0300

    fixed

---
+++ search.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+   <title>Document</title>
+   <link rel="stylesheet" href="css/style.css">
+   <link href="https://fonts.googleapis.com/css?family=Bad+Script:regular" rel="stylesheet" />
+   <link href="https://fonts.googleapis.com/css?family=Amatic+SC:regular,700" rel="stylesheet" />
+   <link href="https://fonts.googleapis.com/css?family=Alumni+Sans:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+   <svg style="display:none">
+      <symbol id="arrow" viewBox="0 0 330.002 330.002">
+         <g>
+            <path d="M327.001,99.751c-4.971-6.628-14.374-7.971-21-3l-140.997,105.75L24.001,96.751
+	c-6.628-4.971-16.029-3.626-21,3c-4.971,6.627-3.627,16.03,3,21l150.004,112.5c2.667,2,5.833,3,9,3c3.166,0,6.333-1,9-3
+	l149.996-112.5C330.628,115.781,331.972,106.379,327.001,99.751z"/>
+         </g>
+      </symbol>
+      <symbol id="basket" viewBox="0 0 24 24">
+         <g>
+            <path fill="#181818" fill-rule="nonzero" d="M5.457 5H3a1 1 0 1 1 0-2h3.273a1 1 0 0 1 .98.8L7.907 7H21a1 1 0 0 1 .982 1.191l-1.311 6.72a2.623 2.623 0 0 1-2.616 2.09L10.12 17c-1.272.024-2.384-.852-2.635-2.087L6.12 8.24a.995.995 0 0 1-.016-.078L5.457 5zm2.86 4l1.128 5.513c.058.283.324.493.657.487h7.971c.314.006.58-.204.636-.48L19.786 9H8.317zM10 19h8a1 1 0 0 1 0 2h-8a1 1 0 0 1 0-2z"></path>
+         </g>
+      </symbol>
+      <symbol id="about" viewBox="0 0 96 96">
+         <g>
+            <path d="M66,84H54V42a5.9966,5.9966,0,0,0-6-6H36a6,6,0,0,0,0,12h6V84H30a6,6,0,0,0,0,12H66a6,6,0,0,0,0-12Z"/><path d="M48,24A12,12,0,1,0,36,12,12.0119,12.0119,0,0,0,48,24Z"/>
+         </g>
+      </symbol>
+      <symbol id="home" viewBox="0 0 26.39 26.39">
+         <g>
+            <path d="M3.588,24.297c0,0-0.024,0.59,0.553,0.59c0.718,0,6.652-0.008,6.652-0.008l0.01-5.451c0,0-0.094-0.898,0.777-0.898h2.761
+			c1.031,0,0.968,0.898,0.968,0.898l-0.012,5.434c0,0,5.628,0,6.512,0c0.732,0,0.699-0.734,0.699-0.734V14.076L13.33,5.913
+			l-9.742,8.164C3.588,14.077,3.588,24.297,3.588,24.297z"/>
+		<path d="M0,13.317c0,0,0.826,1.524,2.631,0l10.781-9.121l10.107,9.064c2.088,1.506,2.871,0,2.871,0L13.412,1.504L0,13.317z"/>
+		<polygon points="23.273,4.175 20.674,4.175 20.685,7.328 23.273,9.525 		"/>
+         </g>
+      </symbol>
+      <symbol id="geo" viewBox="0 0 32 32">
+         <g>
+            <path d="M16,24l-6.09-8.6A8.14,8.14,0,0,1,16,2a8.08,8.08,0,0,1,8,8.13,8.2,8.2,0,0,1-1.8,5.13ZM16,4a6.07,6.07,0,0,0-6,6.13,6.19,6.19,0,0,0,1.49,4L16,20.52,20.63,14A6.24,6.24,0,0,0,22,10.13,6.07,6.07,0,0,0,16,4Z" transform="translate(0 0)"/><circle cx="16" cy="9" r="2"/><path d="M28,12H26v2h2V28H4V14H6V12H4a2,2,0,0,0-2,2V28a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V14A2,2,0,0,0,28,12Z"/>
+         </g>
+      </symbol>
+      <symbol id="phone" viewBox="0 0 20 20">
+         <g>
+            <path d="M17.256 12.253c-.096-.667-.611-1.187-1.274-1.342-2.577-.604-3.223-2.088-3.332-3.734C12.193 7.092 11.38 7 10 7s-2.193.092-2.65.177c-.109 1.646-.755 3.13-3.332 3.734-.663.156-1.178.675-1.274 1.342l-.497 3.442C2.072 16.907 2.962 18 4.2 18h11.6c1.237 0 2.128-1.093 1.953-2.305l-.497-3.442zM10 15.492c-1.395 0-2.526-1.12-2.526-2.5s1.131-2.5 2.526-2.5 2.526 1.12 2.526 2.5-1.132 2.5-2.526 2.5zM19.95 6c-.024-1.5-3.842-3.999-9.95-4C3.891 2.001.073 4.5.05 6s.021 3.452 2.535 3.127c2.941-.381 2.76-1.408 2.76-2.876C5.345 5.227 7.737 4.98 10 4.98s4.654.247 4.655 1.271c0 1.468-.181 2.495 2.76 2.876C19.928 9.452 19.973 7.5 19.95 6z"/>
+         </g>
+      </symbol>
+   </svg>
+   <?php require 'db.php'; ?>
+
+</head>
+<body>
+   <div class="header--up">
+      <div class="header--up__title">твій кращий вибір</div>
+   </div>
+   <header class="header">
+      <div class="container">
+         <div class="header__inner">
+            <div class="header__gender">
+               <div class="header__male ">
+                  <div class="header__title _slider">він
+                     <img src="img/header/arrow.svg" alt="arrow" class="header__arrow">
+                  </div>
+                  <div class="header__male--nav">
+                     <a href="#" class="header__link">всі товари</a>
+                     <a href="#" class="header__link">сумки</a>
+                     <a href="#" class="header__link">кейси</a>
+                     <a href="#" class="header__link">портмане</a>
+                     <a href="#" class="header__link">ремені</a>
+                     <a href="#" class="header__link">парасольки</a>
+                  </div>
+               </div>
+               <div class="header__female">
+                  <div class="header__title _slider">вона
+                     <img src="img/header/arrow.svg" alt="arrow" class="header__arrow">
+                  </div>
+                  <div class="header__female--nav">
+                     <a href="#" class="header__link">всі товари</a>
+                     <a href="#" class="header__link">сумки</a>
+                     <a href="#" class="header__link">кейси</a>
+                     <a href="#" class="header__link">портмане</a>
+                     <a href="#" class="header__link">ремені</a>
+                     <a href="#" class="header__link">парасольки</a>
+                  </div>
+               </div>
+               <a href="#" class="header__mobile">сумки</a>
+               <a href="#" class="header__mobile">кейси</a>
+               <a href="#" class="header__mobile">портмане</a>
+               <a href="#" class="header__mobile">ремені</a>
+               <a href="#" class="header__mobile">парасольки</a>
+            </div>
+               <a class="header__logo" href="#">
+                  <img src="img/header/logo5.png" alt="logo">
+               </a>
+            <div class="header__right">
+               <form class="header__form" action="">
+                  <input class="header__search" placeholder="пошук" type="search" name="search" id="">
+                  <img class="header__btn" src="img/header/search2.svg" alt="пошук">
+               </form>
+               <a class="header__basket" href="#">
+                  <svg class="header__svg">
+                     <use xlink:href="#basket"></use>
+                     <div class="header__count">1</div>
+                  </svg>
+               </a>
+               <div class="header__burger">
+                  <span></span>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
+   <div class="body">
+      <div class="container">
+         <div class="body__inner">
+         </div>
+         
+      </div>
+   </div>
+   <footer class="footer">
+      <div class="footer__line">
+         <div class="container">
+            <div class="footer__inner">
+               <div class="footer__info">
+                  <div class="footer__link">Головна
+                     <svg class="footer__svg">
+                        <use xlink:href="#home"></use>
+                     </svg>
+                  </div>
+                  <div class="footer__link">Про нас
+                     <svg class="footer__svg">
+                        <use xlink:href="#about"></use>
+                     </svg>
+                  </div>
+                  <a target="_blank" href="https://www.google.com/maps/place/%D0%A6%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9+%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D0%BC%D0%B0%D0%B3/@50.906749,34.796622,16z/data=!4m5!3m4!1s0x0:0xdc737b63790a2531!8m2!3d50.9067489!4d34.7966222?hl=ru" class="footer__link">Покровська площа 3
+                     <svg class="footer__svg">
+                        <use xlink:href="#geo"></use>
+                     </svg>
+                  </a>
+                  <a href="tel:+380957748111" class="footer__link">+380957748111
+                     <svg class="footer__svg">
+                        <use xlink:href="#phone"></use>
+                     </svg>
+                  </a>
+               </div>
+               <div class="footer__logo">
+                  <img src="img/footer/logo5.png" alt="footerLogo">
+               </div>
+               <div class="footer__send">
+                  <div class="footer__title">Ми вам передзвонимо</div>
+                  
+                  <form  action="">
+                     <div class="footer__prenumber">+380</div>
+                     
+                     <input class="footer__number" placeholder=" - - - - - - - -" type="tel" name="number" id="">
+                     <input class="footer__btn" type="submit" value="надіслати">
+                  </form>
+               </div>
+
+
+            </div>
+            
+         </div>
+      </div>
+      
+      <div class="footer__coopyright">All Rights Reserved © BORSA, 2022</div>
+      
+   </footer>
+   <script src="jQuery/jquery-3.6.1.min.js"></script>
+   <script src="js/slick.min.js"></script>
+   <script src="js/app.js"></script>
+</body>
+</html>
